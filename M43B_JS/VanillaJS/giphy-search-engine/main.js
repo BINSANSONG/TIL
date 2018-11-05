4d9a6ccf9344226f0bb69c2fb1c3822d47ddae7e
binsansong <qlstks@gmail.com>
2018-11-05 15:55:34 +0900

    Complete the function

--- M43B_JS/VanillaJS/giphy-search-engine/main.js
+++ M43B_JS/VanillaJS/giphy-search-engine/main.js
@@ -1,25 +1,42 @@
 // 1. input 읽는다
 const button = document.querySelector('#js-button');
 const inputArea = document.querySelector('#js-input');
-button.addEventListener('click',()=>{
+button.addEventListener('click', () => {
     const inputValue = inputArea.value;
-    pushToDOM(inputValue);
+    getData(inputValue);
 });
-inputArea.addEventListener('keyup',(e)=>{
-    if(e.which === 13) button.click();
+inputArea.addEventListener('keyup', (e) => {
+    if (e.which === 13) button.click();
 });
 
 
 
 // 2. API로 data받아서 
-const API_KEY = 'Dgek5nDGPqfWqG5FK7GBMkYVbuOgxEQ6';
-let keyword = 'cats';
-const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
-console.log(URL);
 
 
 // 3. 뿌린다
+const getData = (data) => {
+
+
+    const API_KEY = 'Dgek5nDGPqfWqG5FK7GBMkYVbuOgxEQ6';
+    let keyword = data;
+    const URL = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
+
+    const GiphyAJAXCall = new XMLHttpRequest();
+    GiphyAJAXCall.open('GET', URL);
+    GiphyAJAXCall.send();
+    GiphyAJAXCall.addEventListener('load', (e) => {
+        const rawData = e.target.response;
+        document.querySelector('#result-area').innerHTML = '';
+        pushToDOM(JSON.parse(rawData));
+    });
+
+};
+
 const pushToDOM = (data) => {
-    const resultArea = document.querySelector('#result-area');
-    resultArea.innerHTML = data;
+    data.data.forEach(e => {
+        let img = document.createElement('img');
+        img.src = e.images.original.url;
+        document.querySelector('#result-area').append(img);
+    });
 };

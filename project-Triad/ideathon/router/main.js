f641185f4b1052043d51e2baa39ebf7441efeab5
binsansong <qlstks@gmail.com>
2018-11-17 16:32:06 +0900

    Finish ideathon

--- project-Triad/ideathon/router/main.js
+++ project-Triad/ideathon/router/main.js
@@ -2,11 +2,8 @@ function router(app) {
     app.get('/',(req,res)=>{
         res.render('index.html');
     });
-    app.get('/1',(req,res)=>{
-        res.render('index1.html');
-    })
-    app.get('/2',(req,res)=>{
-        res.render('index2.html');
+    app.get('/main',(req,res)=>{
+        res.render('main.html');
     })
 }
 module.exports = router;

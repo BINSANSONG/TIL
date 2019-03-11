8a8a27325ac94addba0c741bb7475692687f4516
BINSANSONG <qlstks@gmail.com>
2019-03-11 11:18:05 +0900

    Edit for export

--- react-native/physical-gallery/src/App.js
+++ react-native/physical-gallery/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-import { createDrawerNavigator, createAppContainer } from "react-navigation";
+import { Text, View, ScrollView } from 'react-native'
+import { createDrawerNavigator, createAppContainer, SafeAreaView, DrawerItems } from "react-navigation";
 import Icon from '@expo/vector-icons/Ionicons';
 
 import HomeStack from './screen/HomeStack';
 import MyRoutineStack from './screen/MyRoutineStack';
 import SettingStack from './screen/SettingStack';
 import InfoStack from "./screen/InfoStack";
+import { Thumbnail } from 'native-base';
+
+const CustomDrawerContentComponent = (props) => (
+  <SafeAreaView>
+    <View style={{height:130,backgroundColor:'lightblue', alignItems:'center', justifyContent:'center'}}>
+      <Thumbnail large source={require('../assets/physicalgallery.png')}/>
+    </View>
+    <ScrollView>
+      <DrawerItems {...props} />
+    </ScrollView>
+  </SafeAreaView>
+);
+
 
 const AppDrawerNavigator = createDrawerNavigator({
   HomeStack:{
@@ -37,6 +50,9 @@ const AppDrawerNavigator = createDrawerNavigator({
       drawerIcon:(<Icon name='md-information-circle-outline' size={30}/>),
     }),
   },
+},{
+  contentComponent:CustomDrawerContentComponent,
+  drawerWidth:220
 });
 
 export default createAppContainer(AppDrawerNavigator);
